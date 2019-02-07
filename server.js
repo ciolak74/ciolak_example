a47196c040a3ff7390045fa51fae23f6cb7fc9fe
ciolak74 <42117544+ciolak74@users.noreply.github.com>
2019-02-07 10:17:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/pagecount', function (req, res) {
       //res.send('{ DEMO_CUSTOMER_FIND: ' + results + '}');
       db.collection('counts').count(function(err, count ){
       console.log('{ DEMO___WXYZ___Query_PAGECOUNT: ' + count + '}');
-      res.send('{ DEMO___ComuneDiBologna___Query_PAGECOUNT: ' + count + '}');
+      res.send('{ DEMO___WXYZ___Query_PAGECOUNT: ' + count + '}');
     });
   } else {
     res.send('{ ERR_pageCount: -1 }' + mongoURL );
